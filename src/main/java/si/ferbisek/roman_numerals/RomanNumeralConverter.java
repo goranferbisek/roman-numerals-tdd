4b14d94943be0d95f0a9fdd0f496cf1d70614576
Goran Ferbišek <goran.ferbisek@gmail.com>
2022-11-13 16:44:20 +0100

    Convert roman numerals up to 10

--- src/main/java/si/ferbisek/roman_numerals/RomanNumeralConverter.java
+++ src/main/java/si/ferbisek/roman_numerals/RomanNumeralConverter.java
@@ -2,6 +2,16 @@ package si.ferbisek.roman_numerals;
 
 public class RomanNumeralConverter {
     public static String convert(int number) {
-        return "I";
+        if (number < 4)
+            return "I".repeat(Math.max(0, number));
+        if (number == 4)
+            return "IV";
+        if (number == 5)
+            return "V";
+        if (number < 9)
+            return convert(5) + convert(number - 5);
+        if (number == 9)
+            return "IX";
+        return "X";
     }
 }
--- src/test/java/si/ferbisek/roman_numerals/RomanNumeralConverterTest.java
+++ src/test/java/si/ferbisek/roman_numerals/RomanNumeralConverterTest.java
@@ -9,4 +9,40 @@ public class RomanNumeralConverterTest {
     void given_latin_1_then_roman_I() {
         Assertions.assertEquals(RomanNumeralConverter.convert(1), "I");
     }
+
+    @Test
+    void given_latin_2_then_roman_II() {
+        Assertions.assertEquals(RomanNumeralConverter.convert(2), "II");
+    }
+
+    @Test
+    void given_latin_3_then_roman_III() {
+        Assertions.assertEquals(RomanNumeralConverter.convert(3), "III");
+    }
+
+    @Test
+    void given_latin_4_then_roman_IV() {
+        Assertions.assertEquals(RomanNumeralConverter.convert(4), "IV");
+    }
+
+    @Test
+    void given_latin_5_then_roman_V() {
+        Assertions.assertEquals(RomanNumeralConverter.convert(5), "V");
+    }
+
+    @Test
+    void given_latin_6_then_roman_VI() {
+        Assertions.assertEquals(RomanNumeralConverter.convert(6), "VI");
+    }
+
+    @Test
+    void given_latin_9_then_roman_IX() {
+        Assertions.assertEquals(RomanNumeralConverter.convert(9), "IX");
+    }
+
+    @Test
+    void given_latin_10_then_roman_X() {
+        Assertions.assertEquals(RomanNumeralConverter.convert(10), "X");
+    }
+
 }
